eebe9e797777358a1171f7e2e0e77dd6b54741c7
arl.2k3 <ariel_pk@outlook.com>
2024-07-24 01:09:35 -0500

    Presentable Miercoles -GPar -GAud

---
+++ public/js/administracionSistema.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Módulo de aseguradoras cargado');
+    showForm('register'); // Mostrar por defecto el formulario de registro
+});
+
+function showForm(formId) {
+    const forms = document.querySelectorAll('form');
+    forms.forEach(form => {
+        form.classList.add('hidden');
+    });
+
+    const formToShow = document.getElementById(formId);
+    if (formToShow) {
+        formToShow.classList.remove('hidden');
+    }
+}
+
